5267bd65236d7dad0375854c735847ffd1ddcc76
Denis Baryshev <dennybaa@gmail.com>
2016-10-14 17:37:07 +0300

    fixing readable (it's not world readable)

--- recipes/default.rb
+++ recipes/default.rb
@@ -11,6 +11,6 @@ include_recipe 'machine::docker_engine'
 include_recipe 'machine::core_containers'
 include_recipe 'machine::consul_kv'
 
-if ::File.readable?(::File.join(node[:machine][:chef_root], 'chef/Berksfile'))
+if ::File.exist?(::File.join(node[:machine][:chef_root], 'chef/Berksfile'))
   include_recipe 'machine::cookbooks'
 end
