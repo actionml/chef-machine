bc96d41fbf5778274fb2819d45c4668b652d261f
Denis Baryshev <dennybaa@gmail.com>
2016-10-15 19:58:21 +0300

    added ssh-agent integration

--- recipes/cookbooks.rb
+++ recipes/cookbooks.rb
@@ -1,11 +1,24 @@
 ## Install chef production cookbooks
 #
+::Chef::Recipe.send(:include, AML::RecipeHelpers)
+
 
 # we expect berkshelf to be allready in chef gems
 berks_bin = "#{Gem.default_bindir}/berks"
+deploy_key = "/root/.ssh/deploy-#{aml.project[:key_name]}"
 
+# Add deploy key into ssh-agent and get env.
+if ::File.exist?(deploy_key)
+  ssh_agent.add_identity(deploy_key)
+  ssh_agent_env = ssh_agent.env
+end
+
+## Install production cookbooks (with ssh env provided).
+#
 execute 'install-prod-cookbooks' do
+  environment ssh_agent_env || {}
   cwd node[:machine][:chef_root]
   command "#{berks_bin} vendor cookbooks"
+
   action :run
 end
