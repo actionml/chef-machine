2a416737eea4c2e0e49d4b8daa666d80ea65912e
Denis Baryshev <dennybaa@gmail.com>
2017-02-27 21:12:45 +0300

    fixed: docker binary passthrough

--- attributes/default.rb
+++ attributes/default.rb
@@ -15,9 +15,10 @@ default['machine']['container_default_options']['log_opts'] = %w(max-size=50m ma
 
 ## Volumes for passing docker binary and socket into
 #
-socket = node['machine']['engine']['listens'].select {|l| l.start_with?('unix://') }.first || ""
-socket = socket.partition("unix://").last
+socket = node['machine']['engine']['listens'].select {|l| l.start_with?('unix://') }.first || ''
+socket = socket.partition('unix://').last
+socket = socket.empty? ? '/var/run/docker.sock' : socket
 default['machine']['docker_passthrough_volumes'] = %W(
-  /var/run/docker.sock:/var/run/docker.sock
-  #{socket}
+  /usr/bin/docker:/usr/bin/docker
+  #{socket}:/var/run/docker.sock
 )
