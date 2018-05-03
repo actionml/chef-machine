a36a6a42849c7d7ee2473cc4eb5ac653a8ecc690
Denis Baryshev <dennybaa@gmail.com>
2018-05-03 17:19:03 +0300

    systemd-resolved support

--- libraries/network.rb
+++ libraries/network.rb
@@ -2,7 +2,12 @@
 module Machine
   module NetworkHelpers
 
-    CONF = '/etc/resolv.conf'
+    # Locations to treat as the resolv.conf file
+    CONF = %w[
+      /run/systemd/resolve/resolv.conf
+      /etc/resolv.conf
+    ]
+
     IP = %r{(?:[01]?\d\d?|2[0-4]\d|25[0-5])\.(?:[01]?\d\d?|2[0-4]\d|25[0-5])\.(?:[01]?\d\d?|2[0-4]\d|25[0-5])\.(?:[01]?\d\d?|2[0-4]\d|25[0-5])}
     FQDN = %r{(([a-zA-Z0-9]|[a-zA-Z0-9][a-zA-Z0-9\-]*[a-zA-Z0-9])\.)*([A-Za-z0-9]|[A-Za-z0-9][A-Za-z0-9\-]*[A-Za-z0-9])*$}
 
@@ -10,17 +15,21 @@ module Machine
     #
     def resolvconf
       conf = Mash.new()
-      if File.exist?(CONF)
-        lines = File.readlines(CONF)
+
+      CONF.each do |resolv_file|
+        next unless File.exist?(resolv_file)
+
+        lines = File.readlines(resolv_file)
         conf[:nameservers] = lines.grep(/^nameserver #{IP}$/).map {|s| s[IP, 0]}
         conf[:searches] = lines.grep(/^search #{FQDN}$/).map {|s| s[FQDN, 0]}
         conf[:domains] = lines.grep(/^domain #{FQDN}$/).map {|s| s[FQDN, 0]}
+        break
       end
-      conf
+      return conf
     end
 
     ## Retrieve host ip addresses, equivalent to hostname -I
-    # 
+    #
     def host_addresses
       interfaces = node['network']['interfaces'].select { |nk, nv| not (nv['flags'] || []).include? 'LOOPBACK' }
       addresses = interfaces.map do |ik, iv|
